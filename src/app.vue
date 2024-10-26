6be0e02b045d46538d4810d2e507c04ce8704863
arsen <keetrussia@mail.ru>
2024-10-26 12:27:50 +0300

    init project

---
+++ src/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router"
+</script>
+
+<template>
+  <header>
+    <div class="bg-black">huy</div>
+  </header>
+  <RouterView />
+</template>
